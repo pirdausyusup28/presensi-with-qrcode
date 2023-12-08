ff6c6f096f58aff2881697296295a4e1d3d00184
Julian Maulana <julianmaulana@JM-Mac.local>
2023-12-08 14:05:04 +0700

    Test mamang

--- application/views/admin/header_new.php
+++ application/views/admin/header_new.php
@@ -84,7 +84,7 @@
                   </g>
                 </svg>
               </span>
-              <span class="app-brand-text demo menu-text fw-bold ms-2">Sneat</span>
+              <span class="app-brand-text demo menu-text fw-bold ms-2">SIABI</span>
             </a>
             <a href="javascript:void(0);" class="layout-menu-toggle menu-link text-large ms-auto d-block d-xl-none">
               <i class="bx bx-chevron-left bx-sm align-middle"></i>
